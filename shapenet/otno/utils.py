87d727df7555ffcb6e0e15916229bd958fe813be
Xinyi-Li-4869 <xyli4869@outlook.com>
2024-11-01 14:23:07 -0700

    Reorganize files into shapenet directory

---
+++ shapenet/otno/utils.py
@@ -0,0 +1,154 @@
+from typing import List, Optional, Union
+from math import prod
+from pathlib import Path
+import torch
+
+import warnings
+
+# normalization, pointwise gaussian
+class UnitGaussianNormalizer:
+    def __init__(self, x, eps=0.00001, reduce_dim=[0], verbose=True):
+        super().__init__()
+
+        msg = ("neuralop.utils.UnitGaussianNormalizer has been deprecated. "
+               "Please use the newer neuralop.datasets.UnitGaussianNormalizer instead.")
+        warnings.warn(msg, DeprecationWarning)
+        n_samples, *shape = x.shape
+        self.sample_shape = shape
+        self.verbose = verbose
+        self.reduce_dim = reduce_dim
+
+        # x could be in shape of ntrain*n or ntrain*T*n or ntrain*n*T
+        self.mean = torch.mean(x, reduce_dim, keepdim=True).squeeze(0)
+        self.std = torch.std(x, reduce_dim, keepdim=True).squeeze(0)
+        self.eps = eps
+
+        if verbose:
+            print(
+                f"UnitGaussianNormalizer init on {n_samples}, reducing over {reduce_dim}, samples of shape {shape}."
+            )
+            print(f"   Mean and std of shape {self.mean.shape}, eps={eps}")
+
+    def encode(self, x):
+        # x = x.view(-1, *self.sample_shape)
+        x -= self.mean
+        x /= self.std + self.eps
+        # x = (x.view(-1, *self.sample_shape) - self.mean) / (self.std + self.eps)
+        return x
+
+    def decode(self, x, sample_idx=None):
+        if sample_idx is None:
+            std = self.std + self.eps  # n
+            mean = self.mean
+        else:
+            if len(self.mean.shape) == len(sample_idx[0].shape):
+                std = self.std[sample_idx] + self.eps  # batch*n
+                mean = self.mean[sample_idx]
+            if len(self.mean.shape) > len(sample_idx[0].shape):
+                std = self.std[:, sample_idx] + self.eps  # T*batch*n
+                mean = self.mean[:, sample_idx]
+
+        # x is in shape of batch*n or T*batch*n
+        # x = (x.view(self.sample_shape) * std) + mean
+        # x = x.view(-1, *self.sample_shape)
+        x *= std
+        x += mean
+
+        return x
+
+    def cuda(self):
+        self.mean = self.mean.cuda()
+        self.std = self.std.cuda()
+        return self
+
+    def cpu(self):
+        self.mean = self.mean.cpu()
+        self.std = self.std.cpu()
+        return self
+
+    def to(self, device):
+        self.mean = self.mean.to(device)
+        self.std = self.std.to(device)
+        return self
+
+class LpLoss(object):
+    def __init__(self, d=2, p=2, size_average=True, reduction=True):
+        super(LpLoss, self).__init__()
+
+        # Dimension and Lp-norm type are postive
+        assert d > 0 and p > 0
+
+        self.d = d
+        self.p = p
+        self.reduction = reduction
+        self.size_average = size_average
+
+    def abs(self, x, y):
+        num_examples = x.size()[0]
+
+        # Assume uniform mesh
+        h = 1.0 / (x.size()[1] - 1.0)
+
+        all_norms = (h ** (self.d / self.p)) * torch.norm(
+            x.view(num_examples, -1) - y.view(num_examples, -1), self.p, 1
+        )
+
+        if self.reduction:
+            if self.size_average:
+                return torch.mean(all_norms)
+            else:
+                return torch.sum(all_norms)
+
+        return all_norms
+
+    def rel(self, x, y):
+        num_examples = x.size()[0]
+
+        diff_norms = torch.norm(
+            x.reshape(num_examples, -1) - y.reshape(num_examples, -1), self.p, 1
+        )
+        y_norms = torch.norm(y.reshape(num_examples, -1), self.p, 1)
+
+        if self.reduction:
+            if self.size_average:
+                return torch.mean(diff_norms / y_norms)
+            else:
+                return torch.sum(diff_norms / y_norms)
+
+        return diff_norms / y_norms
+
+    def __call__(self, x, y):
+        return self.rel(x, y)
+    
+def count_model_params(model):
+    """Returns the total number of parameters of a PyTorch model
+    
+    Notes
+    -----
+    One complex number is counted as two parameters (we count real and imaginary parts)'
+    """
+    return sum(
+        [p.numel() * 2 if p.is_complex() else p.numel() for p in model.parameters()]
+    )
+
+def count_tensor_params(tensor, dims=None):
+    """Returns the number of parameters (elements) in a single tensor, optionally, along certain dimensions only
+
+    Parameters
+    ----------
+    tensor : torch.tensor
+    dims : int list or None, default is None
+        if not None, the dimensions to consider when counting the number of parameters (elements)
+    
+    Notes
+    -----
+    One complex number is counted as two parameters (we count real and imaginary parts)'
+    """
+    if dims is None:
+        dims = list(tensor.shape)
+    else:
+        dims = [tensor.shape[d] for d in dims]
+    n_params = prod(dims)
+    if tensor.is_complex():
+        return 2*n_params
+    return n_params
