25ef9ecc18986945d74b3f2d070e863f022c6a0a
Xinyi-Li-4869 <xyli4869@outlook.com>
2024-11-01 17:08:35 -0700

    Update ot_train.py

--- shapenet/otno/ot_train.py
+++ shapenet/otno/ot_train.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 
 from torch.utils.data import DataLoader, Dataset
-from ..utils import UnitGaussianNormalizer, LpLoss, count_model_params
+from utils import UnitGaussianNormalizer, LpLoss, count_model_params
 from .TransportFNO import TransportFNO
 from timeit import default_timer
 
